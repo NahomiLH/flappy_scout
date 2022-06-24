8608a9ed040f13051c42d79f95574e00643fe112
Victor Cabello <vmeca87@gmail.com>
2022-06-24 18:18:01 -0500

    add click

--- utileriasEscenaPrincipal.js
+++ utileriasEscenaPrincipal.js
@@ -30,6 +30,10 @@ function preparar_jugador(puntaje) {
     jugador.jump(fuerzaDeSalto)
   })
 
+  onClick(() => {
+    jugador.jump(fuerzaDeSalto)
+  })
+
 
   // si el jugador se sale de la pantalla
   jugador.action(() => {
